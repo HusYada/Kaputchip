a012dc961609b99d5525d072edea06b097e353fb
HusYada <h_geyik@outlook.com>
2024-07-22 00:19:17 +0100

    added new player code
    thanks momo!

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,62 +1,126 @@
-// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
-	// Inputs
-	private KeyCode 
-	k_left = KeyCode.A, 
-	k_right = KeyCode.D, 
-	k_up = KeyCode.W, 
-	k_down = KeyCode.S,
-	k_run = KeyCode.LeftShift,
-	k_jump = KeyCode.Space,
-	k_larm = KeyCode.Mouse0,
-	k_rarm = KeyCode.Mouse1;
+    #region Script Variables
 
-	// Player Variables
+    // Inputs
+    private KeyCode
+    k_left = KeyCode.A,
+    k_right = KeyCode.D,
+    k_up = KeyCode.W,
+    k_down = KeyCode.S,
+    k_run = KeyCode.LeftShift,
+    k_jump = KeyCode.Space,
+    k_larm = KeyCode.Mouse0,
+    k_rarm = KeyCode.Mouse1;
+
+    // Player Variables
     public int
     plyr_shields;
     public bool
     has_key;
-	private int 
-	plyr_spd = 2,
-	plyr_jump = 250,
-	plyr_butterfly = 20;
-	private bool
-	grounded,
+    private int
+    plyr_spd = 2,
+    plyr_jump = 250,
+    plyr_butterfly = 20;
+    private bool
+    grounded,
     reeling;
 
-	// Camera Variables
-	private int
-	cam_spd = 8;
-	private float 
-	cam_v, 
-	cam_h;
+    // Camera Variables
+    private int
+    cam_spd = 8;
+    private float
+    cam_v,
+    cam_h;
 
-	// Componenets
-	public GameObject 
-	spraycan,
-	moviereel;
+    // Componenets
+    public GameObject
+    spraycan,
+    moviereel;
     private Rigidbody rb;
     private GameObject cam;
     private LineRenderer lr;
 
+    #endregion
+
     void Start()
     {
-    	rb = GetComponent<Rigidbody>();
-    	lr = GetComponent<LineRenderer>();
-    	cam = GameObject.FindWithTag("MainCamera");
+        rb = GetComponent<Rigidbody>();
+        lr = GetComponent<LineRenderer>();
+        cam = GameObject.FindWithTag("MainCamera");
         // Lock cursor, unlock with Esc
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    private void Update()
     {
-    	// --------------------------------------------------------------------------
-    	// Camera Movement
+        CameraLook();
+        Movement();
+        Jump();
+        UseMovieReel();
+        UseSprayCan();
 
-    	// The camera's X and Y axis is set to the mouse's X and Y position
+        // --------------------------------------------------------------------------
+        // Ground Check
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 0.5f))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // --------------------------------------------------------------------------
+        // Moving method for Movie Reel while using it.
+        MovieReelSwinging();
+
+        // --------------------------------------------------------------------------
+        // Butterfly Body Part - From a distance from the ground, you may float
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 1f))
+        {
+            rb.drag = 0;
+        }
+        else
+        {
+            if (Input.GetKey(k_jump) && !grounded)
+            {
+                rb.drag = plyr_butterfly;
+            }
+            else
+            {
+                rb.drag = 0;
+            }
+        }
+
+        // --------------------------------------------------------------------------
+        // Custom / Interaction Time
+
+        // Ray bnrayOrigin = new Ray(transform.position, cam.transform.forward);
+        // RaycastHit bnhitInfo;
+
+        // if(Physics.Raycast(rayOrigin, out bnhitInfo, Mathf.Infinity)) 
+        // {
+        //     var hitObject = bnhitInfo.collider.GetComponent<Transform>();
+
+        //     if(hitObject)
+        //     {
+        //         //hmm
+        //     }
+        // }
+        // --------------------------------------------------------------------------
+    }
+
+    private void CameraLook()
+    {
+        // The camera's X and Y axis is set to the mouse's X and Y position
         cam_h += Input.GetAxis("Mouse X") * cam_spd;
         cam_v -= Input.GetAxis("Mouse Y") * cam_spd;
         cam.transform.eulerAngles = new Vector3(cam_v, cam_h, 0);
@@ -65,149 +129,95 @@ public class player : MonoBehaviour
         //Cursor.lockState = CursorLockMode.Locked;
 
         // Reset the camera
-        if(Input.GetKey("f")) 
+        if (Input.GetKey("f"))
         {
             cam_h = 0.0f;
             cam_v = 0.0f;
         }
+    }
 
-        // --------------------------------------------------------------------------
-    	// Player Movement
+    private void Movement()
+    {
+        // Momo: Run button detection.
+        plyr_spd = (Input.GetKey(k_run)) ? 4 : 2;
 
-		if(Input.GetKey(k_left)) 
-		{
-			rb.MovePosition(rb.position - cam.transform.right * plyr_spd * Time.fixedDeltaTime);
-		}
-		if(Input.GetKey(k_right)) 
-		{
-			rb.MovePosition(rb.position + cam.transform.right * plyr_spd * Time.fixedDeltaTime);
-		}
-		if(Input.GetKey(k_up)) 
-		{
-			rb.MovePosition(rb.position + cam.transform.forward * plyr_spd * Time.fixedDeltaTime);
-		}
-		if(Input.GetKey(k_down)) 
-		{
-			rb.MovePosition(rb.position - cam.transform.forward * plyr_spd * Time.fixedDeltaTime);
-		}
-
-		// --------------------------------------------------------------------------
-		// Ground Check
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 0.5f))
+        if (Input.GetKey(k_left))
         {
-            grounded = true;
+            rb.MovePosition(rb.position - cam.transform.right * plyr_spd * Time.deltaTime);
         }
-        else
+        if (Input.GetKey(k_right))
         {
-        	grounded = false;
+            rb.MovePosition(rb.position + cam.transform.right * plyr_spd * Time.deltaTime);
         }
-
-        // --------------------------------------------------------------------------
-		// Run Button
-		if(Input.GetKey(k_run))
-		{
-			plyr_spd = 4;
-		} 
-		else 
-		{
-			plyr_spd = 2;
-		}
-
-		// --------------------------------------------------------------------------
-		// Jump Button
-		if(Input.GetKey(k_jump) && grounded)
-		{
-			rb.AddForce(transform.up * plyr_jump);
-		}
-
-		// --------------------------------------------------------------------------
-		// Butterfly Body Part - From a distance from the ground, you may float
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 1f))
+        if (Input.GetKey(k_up))
         {
-        	rb.drag = 0;
+            rb.MovePosition(rb.position + cam.transform.forward * plyr_spd * Time.deltaTime);
         }
-        else
+        if (Input.GetKey(k_down))
         {
-        	if(Input.GetKey(k_jump) && !grounded)
-			{
-				rb.drag = plyr_butterfly;
-			}
-			else 
-			{
-				rb.drag = 0;
-			}
+            rb.MovePosition(rb.position - cam.transform.forward * plyr_spd * Time.deltaTime);
         }
+    }
 
-        // --------------------------------------------------------------------------
-    	// Spray Can Arm Part
-        if(Input.GetKeyDown(k_larm))
+    private void Jump()
+    {
+        if (Input.GetKeyDown(k_jump) && grounded)
         {
-        	rb.AddForce(-cam.transform.forward * plyr_jump);
-        	Quaternion cam_rotation = Quaternion.identity;
-        	cam_rotation.eulerAngles = new Vector3(cam_v, cam_h, 0);
-			Instantiate(spraycan, rb.position + cam.transform.forward, cam_rotation);
+            rb.AddForce(transform.up * plyr_jump);
         }
+    }
 
-        // --------------------------------------------------------------------------
-    	// Movie Reel Arm Part
-    	        
-    	// make it so its turned off when not using
+    private void UseSprayCan()
+    {
+        if (Input.GetKeyDown(k_larm))
+        {
+            rb.AddForce(-cam.transform.forward * plyr_jump);
+            Quaternion cam_rotation = Quaternion.identity;
+            cam_rotation.eulerAngles = new Vector3(cam_v, cam_h, 0);
+            Instantiate(spraycan, rb.position + cam.transform.forward, cam_rotation);
+        }
+    }
+
+    private void UseMovieReel()
+    {
+        if (Input.GetKeyDown(k_rarm))
+        {
+            int layerMask = 1 << 6;
+            layerMask = ~layerMask;
+
+            Ray rayOrigin = new Ray(transform.position, cam.transform.forward);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo, Mathf.Infinity, layerMask))
+            {
+                var hitObject = hitInfo.collider.GetComponent<Transform>();
+
+                if (hitObject)
+                {
+                    //Instantiate(moviereel, hitInfo.point, Quaternion.identity);
+                    moviereel.transform.position = hitInfo.point;
+                    reeling = true;
+                }
+            }
+        }
+    }
+
+    private void MovieReelSwinging()
+    {
         LineRenderer lr = GetComponent<LineRenderer>();
         lr.SetPosition(0, transform.position);
         lr.SetPosition(1, transform.position);
 
-    	if(Input.GetKeyDown(k_rarm))
-    	{
-            int layerMask = 1 << 6;
-            layerMask = ~layerMask;
-
-    		Ray rayOrigin = new Ray(transform.position, cam.transform.forward);
-    		RaycastHit hitInfo;
-
-    		if(Physics.Raycast(rayOrigin, out hitInfo, Mathf.Infinity, layerMask)) 
-    		{
-    			var hitObject = hitInfo.collider.GetComponent<Transform>();
-
-    			if(hitObject)
-    			{
-    				//Instantiate(moviereel, hitInfo.point, Quaternion.identity);
-    				moviereel.transform.position = hitInfo.point;
-                    reeling = true;
-    			}
-    		}
-    	}
-
-        if(Input.GetKey(k_rarm) && reeling)
+        if (Input.GetKey(k_rarm) && reeling)
         {
-        	cam.transform.LookAt(moviereel.transform.position);
-			rb.AddRelativeForce(cam.transform.forward * plyr_jump/10, ForceMode.Force);
-			lr.SetPosition(1, moviereel.transform.position);
+            cam.transform.LookAt(moviereel.transform.position);
+            rb.AddRelativeForce(cam.transform.forward * plyr_jump / 10, ForceMode.Force);
+            lr.SetPosition(1, moviereel.transform.position);
         }
 
-        if(Input.GetKeyUp(k_rarm))
+        if (Input.GetKeyUp(k_rarm))
         {
-        	reeling = false;
+            reeling = false;
         }
-
-		// --------------------------------------------------------------------------
-        // Custom / Interaction Time
-
-        // Ray bnrayOrigin = new Ray(transform.position, cam.transform.forward);
-        // RaycastHit bnhitInfo;
-
-        // if(Physics.Raycast(bnrayOrigin, out bnhitInfo, Mathf.Infinity)) 
-        // {
-        //     if(bnhitInfo.collider.gameObject.tag == "Card")
-        //     {
-        //         if(Input.GetKeyDown(k_larm))
-        //         {
-        //             //bnhitInfo.collider.sd
-        //             print("bnhitInfo.collider.gameObject.name");
-        //         }
-        //     }
-        // }
-
-
-        // --------------------------------------------------------------------------
     }
 }
