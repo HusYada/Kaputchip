188af2812d5a076cea45b0ba9aeacc17dbc6b388
HusYada <h_geyik@outlook.com>
2024-08-29 21:18:01 +0100

    thursday-1
    scary merge

--- Assets/Scripts/game.cs
+++ Assets/Scripts/game.cs
@@ -6,11 +6,46 @@ using TMPro;
 
 public class game : MonoBehaviour
 {
-	public TMP_Text shields;
+	//public TMP_Text shields;
 	//public Image key;
+	// 800 x 600 @ 60.0524401590121Hz
+
+	public GameObject canvo;
+    public Camera cam;
+    public GameObject commandlogmask;
+    public GameObject hpbar;
+    public TMP_Text cmd_tex;
+    public GameObject anothercmdtex1pos;
+    public TMP_Text anothercmdtex1;
+    public TMP_Text anothercmdtex2;
+    public TMP_Text inv_text;
 
     void Start()
     {
-        shields = GetComponent<TMP_Text>();
+        //shields = GetComponent<TMP_Text>();
+
+        // if using crt 800x600 resolution
+        if(Screen.currentResolution.width == 800)
+        {
+            Vector3 cmdcrtpos = new Vector3(1, -582.2082f);
+            Vector2 cmdcrtscl = new Vector3(1935, 208.279f);
+            Vector3 hppos = new Vector3(-460, 660);
+            Vector3 hpscl = new Vector3(6, 6);
+            Vector3 cmdhelptextpos = new Vector3(-679, -680.0998f);
+
+        	//print("working!!");
+        	canvo.GetComponent<CanvasScaler>().scaleFactor = 0.42f;
+            //cam.rect = new Rect(0.85f, 0.85f, 0.15f, 1.0f);
+            cam.rect = new Rect(0.75f, 0.75f, 0.25f, 0.25f);
+            commandlogmask.GetComponent<RectTransform>().localPosition = cmdcrtpos;
+            commandlogmask.GetComponent<RectTransform>().sizeDelta = cmdcrtscl;
+            hpbar.GetComponent<RectTransform>().localPosition = hppos;
+            hpbar.GetComponent<RectTransform>().localScale = hpscl;
+            cmd_tex.GetComponent<TMPro.TextMeshProUGUI>().fontSize = 72;
+            anothercmdtex1pos.GetComponent<RectTransform>().localPosition = cmdhelptextpos;
+            anothercmdtex1.GetComponent<TMPro.TextMeshProUGUI>().fontSize = 56;
+            anothercmdtex2.GetComponent<TMPro.TextMeshProUGUI>().fontSize = 72;
+            inv_text.GetComponent<TMPro.TextMeshProUGUI>().fontSize = 72;
+        }
     }
 }
