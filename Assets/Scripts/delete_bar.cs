95c86d39e077d5cff33155f2a93d70eb0ab27f31
momouang <syes0923momo@gmail.com>
2024-09-02 13:00:39 +0100

    add sound effect file / change material / character head turing

--- Assets/Scripts/delete_bar.cs
+++ Assets/Scripts/delete_bar.cs
@@ -6,24 +6,29 @@ using TMPro;
 
 public class delete_bar : MonoBehaviour
 {
-	/*
+    /*
 	10 > 9
 	0.95 > 1.45
 	-1
 	+0.5
 	*/
 
-	public float
-	bar_start,
-	bar_end,
-	bar_startpos,
-	bar_endpos,
-	bar_spd,
-	bar_scale;
+    public float
+    bar_start,
+    bar_end,
+    bar_startpos,
+    bar_endpos,
+    bar_spd,
+    bar_scale;
 
-	public GameObject mousywousy, fire_scene;
+    public GameObject mousywousy, fire_scene;
     public player plyr;
-	public TMP_Text progress;
+    public TMP_Text progress;
+
+    public Material fireMaterial;
+    public GameObject[] walls;
+
+    public Animator hackerAnim;
 
     void Start()
     {
@@ -33,29 +38,50 @@ public class delete_bar : MonoBehaviour
 
     void Update()
     {
-        if(mousywousy.GetComponent<mouse>().pushing_d_bar == false && transform.localScale.x < 10)
+        if (mousywousy.GetComponent<mouse>().pushing_d_bar == false && transform.localScale.x < 10)
         {
-        	Vector3 scaleChange = new Vector3(bar_scale, 0, 0);
-        	//hmm
-        	transform.position = Vector3.MoveTowards(transform.position, 
-        	new Vector3(bar_endpos, transform.position.y, transform.position.z), bar_spd * Time.deltaTime);
-        	transform.localScale += scaleChange;
-        	progress.text = (int)(transform.localScale.x*10) + "%";
+            Vector3 scaleChange = new Vector3(bar_scale, 0, 0);
+            //hmm
+            transform.position = Vector3.MoveTowards(transform.position,
+            new Vector3(bar_endpos, transform.position.y, transform.position.z), bar_spd * Time.deltaTime);
+            transform.localScale += scaleChange;
+            progress.text = (int)(transform.localScale.x * 10) + "%";
         }
-        if(mousywousy.GetComponent<mouse>().pushing_d_bar == true && transform.localScale.x > 0)
+        if (mousywousy.GetComponent<mouse>().pushing_d_bar == true && transform.localScale.x > 0)
         {
-        	Vector3 scaleChange = new Vector3(bar_scale, 0, 0);
-        	//hmm
-        	transform.position = Vector3.MoveTowards(transform.position, 
-        	new Vector3(bar_startpos, transform.position.y, transform.position.z), bar_spd * Time.deltaTime);
-        	transform.localScale -= scaleChange;
-        	progress.text = (int)(transform.localScale.x*10) + "%";
+            Vector3 scaleChange = new Vector3(bar_scale, 0, 0);
+            //hmm
+            transform.position = Vector3.MoveTowards(transform.position,
+            new Vector3(bar_startpos, transform.position.y, transform.position.z), bar_spd * Time.deltaTime);
+            transform.localScale -= scaleChange;
+            progress.text = (int)(transform.localScale.x * 10) + "%";
         }
-        if(transform.localScale.x >= 10)
+        if (transform.localScale.x >= 10)
         {
-            transform.localScale = new Vector3(-1000, -1000, -1000);;
+            transform.localScale = new Vector3(-1000, -1000, -1000); ;
             Instantiate(fire_scene);
-            plyr.ShakeCamera(5, 5);
+            ChangeMaterial();
+            hackerAnim.SetTrigger("playAnim");
+            plyr.ShakeCamera(8, 1);
+        }
+    }
+
+    private void ChangeMaterial()
+    {
+        foreach (GameObject go in walls)
+        {
+            Material[] materials = go.GetComponent<MeshRenderer>().sharedMaterials;
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i] = fireMaterial;
+            }
+            go.GetComponent<Renderer>().sharedMaterials = materials;
+
+            materials = go.GetComponent<Renderer>().materials;
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i] = fireMaterial;
+            }
         }
     }
 }
